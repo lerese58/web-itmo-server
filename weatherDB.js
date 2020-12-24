b898f7e5b648d553e1d1cc601e44c7cf8857bc6c
lerese58 <lerese58@gmail.com>
2020-12-24 08:33:55 +0300

    transfer db to localhost

--- weatherDB.js
+++ weatherDB.js
@@ -1,7 +1,7 @@
 const pgp = require('pg-promise')();
 
 // it's not dangerous -- access to db is available only from localhost
-const weatherDB = pgp('postgres://weatheruser:lerese58@192.168.56.57:5432/weather')
+const weatherDB = pgp('postgres://postgres:postgres@localhost:5432/postgres')
 
 
 module.exports = weatherDB;
